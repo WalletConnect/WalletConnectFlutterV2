cb21afc520b976ba64550f6438688c0cf7daca25
Sterling Long <sterling.long@lztek.io>
2023-02-21 18:26:40 -0700

    Added generated files back

---
+++ lib/apis/models/json_rpc_request.g.dart
@@ -0,0 +1,23 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+
+part of 'json_rpc_request.dart';
+
+// **************************************************************************
+// JsonSerializableGenerator
+// **************************************************************************
+
+JsonRpcRequest _$JsonRpcRequestFromJson(Map<String, dynamic> json) =>
+    JsonRpcRequest(
+      id: json['id'] as int,
+      jsonrpc: json['jsonrpc'] as String? ?? '2.0',
+      method: json['method'] as String,
+      params: json['params'],
+    );
+
+Map<String, dynamic> _$JsonRpcRequestToJson(JsonRpcRequest instance) =>
+    <String, dynamic>{
+      'id': instance.id,
+      'jsonrpc': instance.jsonrpc,
+      'method': instance.method,
+      'params': instance.params,
+    };
