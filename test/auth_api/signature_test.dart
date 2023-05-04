fe4885731ab4edd7d1908ee7d28c7e45f17030b9
Sterling Long <sterling.long@lztek.io>
2023-05-04 12:35:39 -0600

    Updated signature validation, it's still not working with other examples, but I'm not sure how to fix it

--- test/auth_api/signature_test.dart
+++ test/auth_api/signature_test.dart
@@ -8,6 +8,33 @@ void main() {
   TestWidgetsFlutterBinding.ensureInitialized();
 
   group('AuthSignature', () {
+    // test('signatures recover the public key of the signer', () {
+    //   final messages = [
+    //     'Hello world',
+    //     'UTF8 chars ©âèíöu ∂øµ€',
+    //     '🚀✨🌎',
+    //     DateTime.now().toString()
+    //   ];
+    //   final privateKeys = [
+    //     '3c9229289a6125f7fdf1885a77bb12c37a8d3b4962d936f7e3084dece32a3ca1',
+    //     'a69ab6a98f9c6a98b9a6b8e9b6a8e69c6ea96b5050eb77a17e3ba685805aeb88',
+    //     'ca7eb9798e79c8799ea79aec7be98a7b9a7c98ae7b98061a53be764a85b8e785',
+    //     '192b519765c9589a6b8c9a486ab938cba9638ab876056237649264b9cb96d88f',
+    //     'b6a8f6a96931ad89d3a98e69ad6b98794673615b74675d7b5a674ba82b648a6d'
+    //   ];
+
+    //   for (final message in messages) {
+    //     final messageHash = keccak256(Uint8List.fromList(utf8.encode(message)));
+
+    //     for (final privateKey in privateKeys) {
+    //       final publicKey = privateKeyBytesToPublic(hexToBytes(privateKey));
+    //       final signature = sign(messageHash, hexToBytes(privateKey));
+
+    //       final recoveredPublicKey = ecRecover(messageHash, signature);
+    //       expect(bytesToHex(publicKey), bytesToHex(recoveredPublicKey));
+    //     }
+    //   }
+    // });
     test('isValidEip191Signature', () async {
       // print('Actual Sig: ${EthSigUtil.signPersonalMessage(
       //   message: Uint8List.fromList(TEST_MESSAGE_EIP191.codeUnits),
