dda9dc9dce4efb0938ba0e1e53943f93f845fe08
Alfreedom <00tango.bromine@icloud.com>
2023-10-25 09:37:34 +0200

    Added bundleId/packageName into websocket handshake connection

--- example/wallet/macos/Flutter/GeneratedPluginRegistrant.swift
+++ example/wallet/macos/Flutter/GeneratedPluginRegistrant.swift
@@ -6,9 +6,11 @@ import FlutterMacOS
 import Foundation
 
 import mobile_scanner
+import package_info_plus
 import shared_preferences_foundation
 
 func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
   MobileScannerPlugin.register(with: registry.registrar(forPlugin: "MobileScannerPlugin"))
+  FPPPackageInfoPlusPlugin.register(with: registry.registrar(forPlugin: "FPPPackageInfoPlusPlugin"))
   SharedPreferencesPlugin.register(with: registry.registrar(forPlugin: "SharedPreferencesPlugin"))
 }
